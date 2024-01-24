c74ffddf4f4371cf4510f429103b5ec1ca9886de
Ghasem <ghasem.elyasi@gmail.com>
2024-01-24 07:47:23 +0100

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Raouf on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
